b3c66130a91a036de0806a0f8b010bfb22fabaef
Hexilee <hexileee@gmail.com>
2018-09-30 13:47:18 +0800

    remove unmarshalers

--- unmarshalers/utils.go
+++
@@ -1,8 +0,0 @@
-package unmarshalers
-
-import "reflect"
-
-// cannot use it for reference kind (Ptr, Interface, Func, Map, Slice)
-func isZero(v interface{}) bool {
-	return v == reflect.Zero(reflect.TypeOf(v)).Interface()
-}
