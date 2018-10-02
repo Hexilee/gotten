515f4236c9f80a1ecf44bb1d146f5b7bd7c89851
Hexilee <hexileee@gmail.com>
2018-10-02 14:30:24 +0800

    add UnmarshalFunc

--- Unmarshaler.go
+++ Unmarshaler.go
@@ -4,4 +4,10 @@ type (
 	Unmarshaler interface {
 		Unmarshal(data []byte, v interface{}) error
 	}
+
+	UnmarshalFunc func(data []byte, v interface{}) error
 )
+
+func (fn UnmarshalFunc) Unmarshal(data []byte, v interface{}) error {
+	return fn(data, v)
+}
