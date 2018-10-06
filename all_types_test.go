10fe33086e642d82d3157834573294e4da639721
Hexilee <hexileee@gmail.com>
2018-10-06 15:11:50 +0800

    MultipartRequest pass all tests

--- all_types_test.go
+++ all_types_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -51,10 +52,41 @@ func TestFormRequest(t *testing.T) {
 	assert.Equal(t, "/form", req.URL.Path)
 }
 
+func TestMultipartRequest(t *testing.T) {
+	creator, err := gotten.NewBuilder().
+		SetBaseUrl("https://mock.io").
+		SetClient(mockClient).
+		Build()
+	assert.Nil(t, err)
+	var service AllTypesService
+	assert.Nil(t, creator.Impl(&service))
+	req, err := service.MultipartRequest(&MultipartParams{
+		JsonBeforeForm: TestSerializationObject,
+		Int:            TestInt,
+		String:         TestString,
+		Stringer:       TestStringer,
+		Reader:         getTestReader(),
+		JsonAfterForm:  TestSerializationObject,
+		XmlAfterForm:   TestSerializationObject,
+	})
+	assert.Nil(t, err)
+	req.ParseMultipartForm(2 << 32)
+	assert.Equal(t, TestJSON, req.PostFormValue("json_before_form"))
+	assert.Equal(t, TestJSON, req.PostFormValue("json_after_form"))
+	assert.Equal(t, TestXML, req.PostFormValue("xml_after_form"))
+	assert.Equal(t, TestString, req.PostFormValue("int"))
+	assert.Equal(t, TestString, req.PostFormValue("string"))
+	assert.Equal(t, TestString, req.PostFormValue("stringer"))
+	assert.Equal(t, TestString, req.PostFormValue("reader"))
+	assert.True(t, strings.HasPrefix(req.Header.Get(headers.HeaderContentType), headers.MIMEMultipartForm))
+	assert.Equal(t, "/multipart", req.URL.Path)
+}
+
 type (
 	AllTypesService struct {
 		FormParamsRequest            func(*FormParams) (*http.Request, error)                        `method:"POST" path:"/form"`
 		FormParamsWithDefaultRequest func(withDefault *FormParamsWithDefault) (*http.Request, error) `method:"POST" path:"/form"`
+		MultipartRequest             func(*MultipartParams) (*http.Request, error)                   `method:"POST" path:"/multipart"`
 	}
 
 	FormParams struct {
@@ -73,6 +105,16 @@ type (
 		Stringer       fmt.Stringer         `type:"form" default:"1"`
 		JsonAfterForm  *SerializationStruct `type:"json" default:"{\"data\":\"1\"}"`
 		XmlAfterForm   *SerializationStruct `type:"xml" default:"<SerializationStruct><Data>1</Data></SerializationStruct>"`
+	}
+
+	MultipartParams struct {
+		JsonBeforeForm *SerializationStruct `type:"json"`
+		Int            int                  `type:"part"`
+		String         string               `type:"part"`
+		Stringer       fmt.Stringer         `type:"part"`
+		Reader         io.Reader            `type:"part"`
+		JsonAfterForm  *SerializationStruct `type:"json"`
+		XmlAfterForm   *SerializationStruct `type:"xml" `
 	}
 
 	SerializationStruct struct {
