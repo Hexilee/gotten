05c73ad95af4079330511df013891da388abe1ad
Hexilee <hexileee@gmail.com>
2018-10-06 21:41:10 +0800

    add exception tests for RequestField.go

--- exception_test.go
+++ exception_test.go
@@ -2,6 +2,7 @@ package gotten
 
 import (
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -21,4 +22,40 @@ func TestGetReaderGetterFunc(t *testing.T) {
 	_, err := getReaderGetterFunc(FilePathType, TypeJSON)
 	assert.Error(t, err)
 	assert.Equal(t, UnsupportedFieldTypeError(FilePathType, TypeJSON), err)
+}
+
+func TestGetValueFromFilePath(t *testing.T) {
+	reader := 1
+	defer func() {
+		err := recover()
+		assert.Equal(t, reflect.TypeOf(reader).String()+" is not FilePath", err)
+	}()
+	getValueFromFilePath(reflect.ValueOf(reader))
+}
+
+func TestGetValueFromString(t *testing.T) {
+	reader := 1
+	defer func() {
+		err := recover()
+		assert.Equal(t, ValueIsNotStringError(reflect.TypeOf(reader)), err)
+	}()
+	getValueFromString(reflect.ValueOf(reader))
+}
+
+func TestGetValueFromInt(t *testing.T) {
+	reader := "1"
+	defer func() {
+		err := recover()
+		assert.Equal(t, ValueIsNotIntError(reflect.TypeOf(reader)), err)
+	}()
+	getValueFromInt(reflect.ValueOf(reader))
+}
+
+func TestGetReaderFromString(t *testing.T) {
+	reader := 1
+	defer func() {
+		err := recover()
+		assert.Equal(t, ValueIsNotStringError(reflect.TypeOf(reader)), err)
+	}()
+	getReaderFromString(reflect.ValueOf(reader))
 }
