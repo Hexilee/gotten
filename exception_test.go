8e58a2a83cce0354c04e8e04b5d9f24856e2ee64
Hexilee <hexileee@gmail.com>
2018-10-06 21:33:43 +0800

    add exception_test.go

--- exception_test.go
+++ exception_test.go
@@ -1,1 +1,24 @@
 package gotten
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetMultipartValueGetterFunc(t *testing.T) {
+	_, err := getMultipartValueGetterFunc(ReaderType, TypeMultipart)
+	assert.Error(t, err)
+	assert.Equal(t, UnsupportedFieldTypeError(ReaderType, TypeMultipart), err)
+}
+
+func TestGetValueGetterFunc(t *testing.T) {
+	_, err := getValueGetterFunc(ReaderType, TypePath)
+	assert.Error(t, err)
+	assert.Equal(t, UnsupportedFieldTypeError(ReaderType, TypePath), err)
+}
+
+func TestGetReaderGetterFunc(t *testing.T) {
+	_, err := getReaderGetterFunc(FilePathType, TypeJSON)
+	assert.Error(t, err)
+	assert.Equal(t, UnsupportedFieldTypeError(FilePathType, TypeJSON), err)
+}
